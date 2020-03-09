d4d060f2ac81292e84f73d980d76a029a5cf351b
吴为 <woo@iMac.local>
2020-03-09 20:54:08 +0800

    Create 5.0.79.tar.gz

--- build.sh
+++ build.sh
@@ -12,7 +12,6 @@ if [[ ! -f $VER.tar.gz || ! -s $VER.tar.gz  ]] ; then
 fi
 
 if [[ $? -eq 0 ]] ; then
-
 	rm -rf metasploit-framework-$VER
 	if ! tar -zxf $VER.tar.gz >/dev/null 2>&1 ; then
 		echo -e "\033[1;34m [-] $VER.tar.gz corrupts. Remove it and try again.\033[0m"
@@ -21,8 +20,9 @@ if [[ $? -eq 0 ]] ; then
 
 	cd metasploit-framework-$VER
 	cp -f ../Dockerfile .
+	cp -f ../hashdump.patch hashdump.patch
 	cp -f ../entrypoint.sh docker/entrypoint.sh
-
+	
 	if which docker >/dev/null 2>&1 ; then
 		echo -e "\033[1;34m [+] Start to build image...\033[0m"
 		docker build . -t msf:$VER
